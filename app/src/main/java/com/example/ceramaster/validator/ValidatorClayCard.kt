abf35dfdd5feecd96c3e613212310a7b31b2e3b8
PolinaRrr <poniapocalypsisa@gmail.com>
2023-06-19 16:25:08 +0300

    # validation in progress

--- app/src/main/java/com/example/ceramaster/validator/ValidatorClayCard.kt
+++ app/src/main/java/com/example/ceramaster/validator/ValidatorClayCard.kt
@@ -6,18 +6,27 @@ import com.example.ceramaster.ValidationCard
 class ValidatorClayCard(
     private val params: Map<EditText, Map<String, Int>>,
     private val valueClayCard: ClayCardValue
-):ValidationCard {
+) : ValidationCard {
 
-
+/*
+  этот метод должен пройтись по мапае и для каждого ключа провести проверку по параметрам в мапе значений с введенными значениями из объекта дто
+  метод возвращает мапу, где ключ это поле, а значение - результат прохождения всех проверок на это поле, если хоть одна не пройдена, то валидация неудачная
+ */
     override fun validateCard(
         params: Map<EditText, Map<String, Int>>,
         valueClayCard: ClayCardValue
     ): Map<EditText, Boolean> {
-        val result: Map<EditText, Boolean> = mapOf()
 
+    params.forEach{}
+
+        val result: Map<EditText, Boolean> = mapOf()
 
         return result
     }
 
+//
+//    private fun comparisonOfValues(valueClayCard: ClayCardValue,): Boolean{
+//
+//    }
 
 }
